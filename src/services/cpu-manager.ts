a28d6e5ec9760658ead15d56b26f149fdc998629
roger <rogersteam1402@gmail.com>
2025-10-17 20:06:41 +0200

    fix(cpu): fix cache logic in cpu

--- src/services/cpu-manager.ts
+++ src/services/cpu-manager.ts
@@ -1,36 +1,28 @@
+import EventEmitter from "eventemitter3";
 import { hexTo4BitBinary } from "../utils/convert";
 import { CacheFail, CacheManager } from "./cache-manager";
 import { MemoryManager } from "./memory-manager";
 
-export class CPUManager {
+export class CPUManager extends EventEmitter {
   cacheManager: CacheManager;
   memoryManager: MemoryManager;
 
   constructor() {
+    super();
     this.cacheManager = new CacheManager();
     this.memoryManager = new MemoryManager();
   }
 
   executeSearch(hexInput: string) {
-    let string: string = "";
-    for (let letter of hexInput) {
-      string += hexTo4BitBinary(letter);
-    }
-
-    const query: CacheRegister = {
-      tag: string.substring(0, 9),
-      index: parseInt(string.substring(9, 23), 2),
-      word: string.substring(23, 25),
-    };
+    let string: string = hexTo4BitBinary(hexInput);
 
     try {
-      this.cacheManager.receiveCacheRegister(query);
+      this.cacheManager.getWord(hexInput);
     } catch (e) {
-      console.log("entro");
       if (e instanceof CacheFail) {
-        console.log("entro x2");
-        const { word, blockId } = this.memoryManager.getWord(query.tag);
-        this.cacheManager.setRegister(blockId, word, query.tag);
+        const [word, block] = this.memoryManager.getWord(hexInput);
+        this.emit("word", word);
+        this.cacheManager.setRegister(block);
       }
     }
   }
