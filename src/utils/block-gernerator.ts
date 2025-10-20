59572c9cc04e80ff94ba4494b4f7371723ce63af
roger <rogersteam1402@gmail.com>
2025-10-20 03:32:42 +0200

    feat(utils): add generate-strings functions

--- src/utils/block-gernerator.ts
+++ src/utils/block-gernerator.ts
@@ -1,26 +1,53 @@
 export function generarCadenasUnicas(cantidad: number): string[] {
   const caracteres = "ABCDEF0123456789";
-  const longitudTag = 2;
-  const longitudCadena = 6;
+  const longitudCadena = 8;
   const conjunto = new Set<string>();
-  const conjuntoTags = new Set<string>();
 
-  while (conjuntoTags.size < cantidad) {
-    let tag = "";
+  while (conjunto.size < cantidad) {
     let cadena = "";
-    for (let i = 0; i < longitudTag; i++) {
-      const indice = Math.floor(Math.random() * caracteres.length);
-      tag += caracteres[indice];
-    }
-    conjuntoTags.add(tag);
 
     for (let i = 0; i < longitudCadena; i++) {
       const indice = Math.floor(Math.random() * caracteres.length);
       cadena += caracteres[indice];
     }
-
-    conjunto.add(tag + cadena);
+    conjunto.add(cadena);
   }
 
   return Array.from(conjunto);
+}
+
+export function generarTagsUnicos(cantidad: number): string[] {
+  const caracteres = "ABCDEF0123456789";
+  const longitudTag = 2;
+  const conjuntoTags = new Set<string>();
+
+  while (conjuntoTags.size < cantidad) {
+    let tag = "";
+    for (let i = 0; i < longitudTag; i++) {
+      const indice = Math.floor(Math.random() * caracteres.length);
+      tag += caracteres[indice];
+    }
+    conjuntoTags.add(tag);
+  }
+
+  return Array.from(conjuntoTags);
+}
+
+export function generarCuerpos(cantidad: number): string[] {
+  const caracteres = "ABCDEF0123456789";
+  const longitud = 6;
+  const arrayStrings: string[] = [];
+
+  let i = 0;
+  while (i < cantidad) {
+    let cuerpo = "";
+    for (let i = 0; i < longitud; i++) {
+      const indice = Math.floor(Math.random() * caracteres.length);
+      cuerpo += caracteres[indice];
+    }
+    i++;
+    arrayStrings.push(cuerpo);
+  }
+
+  return arrayStrings;
 }
